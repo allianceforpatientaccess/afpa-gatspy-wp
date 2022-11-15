4220fe9e8d3da648c136f20a238ee70dded28166
Avi Nerenberg <devavmeister@gmail.com>
2022-11-14 23:44:24 -0500

    feat(page): [WIP] new ICER page
    - renames old CPTs/pages to make way for new page (according to [this convo](https://teams.microsoft.com/l/message/19:842feb5e845d4e5ba67af579862c390d@thread.tacv2/1667777004137?tenantId=da1bd34e-2618-43b8-9153-821b4a5fc803&groupId=708c650f-dab3-427d-8951-42f16cddeb68&parentMessageId=1666724971690&teamName=Woodberry%20Websites&channelName=AfPA%20Website&createdTime=1667777004137&allowXTenantAccess=false))
    - spelling fixes
    - SonarLint fixes (incl. EOL char & tabs -> spaces, so big diff)

--- wp-content/themes/afpa-gatsby-wp/includes/custom-post-types.php
+++ wp-content/themes/afpa-gatsby-wp/includes/custom-post-types.php
@@ -4,7 +4,7 @@
  * Adds custom post types (CPTs).
  *
  * @param mixed $name the name (capitalized) of the post type
- * @param mixed $slug the slug (no caps, hypenated) of the post type
+ * @param mixed $slug the slug (no caps, hyphenated) of the post type
  * @param bool $hasPluralForm whether or not the CPT should be pluralized
  * @param bool $parent the top-level menu item the CPT will go under
  * @return void
@@ -14,114 +14,115 @@
  */
 function cpt_factory($name, $slug, $hasPluralForm = true, $parent = true)
 {
-	if ($hasPluralForm == true) { // e.g. "Infographics"
-		$labels = array(
-			'name' => _x("{$name}s", 'post type general name'/*, 'your-plugin-textdomain'*/),
-			'singular_name' => _x($name, 'post type singular name'/*, 'your-plugin-textdomain'*/),
-			'menu_name' => _x("{$name}s", 'admin menu'/*, 'your-plugin-textdomain'*/),
-			'name_admin_bar' => _x($name, 'add new on admin bar'/*, 'your-plugin-textdomain'*/),
-			'add_new' => _x('Add New', $slug/*, 'your-plugin-textdomain'*/),
-			'add_new_item' => __("Add New {$name}"/*, 'your-plugin-textdomain'*/),
-			'new_item' => __("New {$name}"/*, 'your-plugin-textdomain'*/),
-			'edit_item' => __("Edit {$name}"/*, 'your-plugin-textdomain'*/),
-			'view_item' => __("View {$name}"/*, 'your-plugin-textdomain'*/),
-			'all_items' => __("All {$name}s"/*, 'your-plugin-textdomain'*/),
-			'search_items' => __("Search {$name}s"/*, 'your-plugin-textdomain'*/),
-			'parent_item_colon' => __("Parent {$name}s:"/*, 'your-plugin-textdomain'*/),
-			'not_found' => __("No {$name}s found."/*, 'your-plugin-textdomain'*/),
-			'not_found_in_trash' => __("No {$name}s found in Trash"/*, 'your-plugin-textdomain'*/),
-		);
+  if ($hasPluralForm) { // e.g. "Infographics"
+    $labels = array(
+      'name' => _x("{$name}s", 'post type general name'),
+      'singular_name' => _x($name, 'post type singular name'),
+      'menu_name' => _x("{$name}s", 'admin menu'),
+      'name_admin_bar' => _x($name, 'add new on admin bar'),
+      'add_new' => _x('Add New', $slug),
+      'add_new_item' => __("Add New {$name}"),
+      'new_item' => __("New {$name}"),
+      'edit_item' => __("Edit {$name}"),
+      'view_item' => __("View {$name}"),
+      'all_items' => __("All {$name}s"),
+      'search_items' => __("Search {$name}s"),
+      'parent_item_colon' => __("Parent {$name}s:"),
+      'not_found' => __("No {$name}s found."),
+      'not_found_in_trash' => __("No {$name}s found in Trash"),
+    );
 
-		$args = array(
-			'labels' => $labels,
-			'description' => __('Description.'/*, 'your-plugin-textdomain'*/),
-			'public' => true,
-			'publicly_queryable' => true,
-			'show_ui' => true,
-			'show_in_menu' => $parent,
-			'query_var' => true,
-			'rewrite' => array('slug' => $slug),
-			'capability_type' => 'post',
-			'has_archive' => true,
-			'hierarchical' => false,
-			'menu_position' => null,
-			'show_in_rest' => true,
-			'rest_base' => "${slug}s",
-			'rest_controller_class' => 'WP_REST_Posts_Controller',
-			'supports' => array('title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments'),
-			'map_meta_cap' => true,
-		);
-	} else { // e.g. "Leadership"
-		$labels = array(
-			'name' => _x($name, 'post type general name'/*, 'your-plugin-textdomain'*/),
-			'singular_name' => _x($name, 'post type singular name'/*, 'your-plugin-textdomain'*/),
-			'menu_name' => _x($name, 'admin menu'/*, 'your-plugin-textdomain'*/),
-			'name_admin_bar' => _x($name, 'add new on admin bar'/*, 'your-plugin-textdomain'*/),
-			'add_new' => _x('Add New', $slug/*, 'your-plugin-textdomain'*/),
-			'add_new_item' => __("Add New {$name}"/*, 'your-plugin-textdomain'*/),
-			'new_item' => __("New {$name}"/*, 'your-plugin-textdomain'*/),
-			'edit_item' => __("Edit {$name}"/*, 'your-plugin-textdomain'*/),
-			'view_item' => __("View {$name}"/*, 'your-plugin-textdomain'*/),
-			'all_items' => __("All {$name}"/*, 'your-plugin-textdomain'*/),
-			'search_items' => __("Search {$name}"/*, 'your-plugin-textdomain'*/),
-			'parent_item_colon' => __("Parent {$name}:"/*, 'your-plugin-textdomain'*/),
-			'not_found' => __("No {$name} found."/*, 'your-plugin-textdomain'*/),
-			'not_found_in_trash' => __("No {$name} found in Trash"/*, 'your-plugin-textdomain'*/),
-		);
+    $args = array(
+      'labels' => $labels,
+      'description' => __('Description.'),
+      'public' => true,
+      'publicly_queryable' => true,
+      'show_ui' => true,
+      'show_in_menu' => $parent,
+      'query_var' => true,
+      'rewrite' => array('slug' => $slug),
+      'capability_type' => 'post',
+      'has_archive' => true,
+      'hierarchical' => false,
+      'menu_position' => null,
+      'show_in_rest' => true,
+      'rest_base' => "${slug}s",
+      'rest_controller_class' => 'WP_REST_Posts_Controller',
+      'supports' => array('title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments'),
+      'map_meta_cap' => true,
+    );
+  } else { // e.g. "Leadership"
+    $labels = array(
+      'name' => _x($name, 'post type general name'),
+      'singular_name' => _x($name, 'post type singular name'),
+      'menu_name' => _x($name, 'admin menu'),
+      'name_admin_bar' => _x($name, 'add new on admin bar'),
+      'add_new' => _x('Add New', $slug),
+      'add_new_item' => __("Add New {$name}"),
+      'new_item' => __("New {$name}"),
+      'edit_item' => __("Edit {$name}"),
+      'view_item' => __("View {$name}"),
+      'all_items' => __("All {$name}"),
+      'search_items' => __("Search {$name}"),
+      'parent_item_colon' => __("Parent {$name}:"),
+      'not_found' => __("No {$name} found."),
+      'not_found_in_trash' => __("No {$name} found in Trash"),
+    );
 
-		$args = array(
-			'labels' => $labels,
-			'description' => __('Description.'/*, 'your-plugin-textdomain'*/),
-			'public' => true,
-			'publicly_queryable' => true,
-			'show_ui' => true,
-			'show_in_menu' => $parent,
-			'query_var' => true,
-			'rewrite' => array('slug' => $slug),
-			'capability_type' => 'post',
-			'has_archive' => true,
-			'hierarchical' => false,
-			'show_in_rest' => true,
-			'rest_base' => $slug,
-			'rest_controller_class' => 'WP_REST_Posts_Controller',
-			'supports' => array('title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments'),
-			'map_meta_cap' => true,
-		);
-	}
+    $args = array(
+      'labels' => $labels,
+      'description' => __('Description.'),
+      'public' => true,
+      'publicly_queryable' => true,
+      'show_ui' => true,
+      'show_in_menu' => $parent,
+      'query_var' => true,
+      'rewrite' => array('slug' => $slug),
+      'capability_type' => 'post',
+      'has_archive' => true,
+      'hierarchical' => false,
+      'show_in_rest' => true,
+      'rest_base' => $slug,
+      'rest_controller_class' => 'WP_REST_Posts_Controller',
+      'supports' => array('title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments'),
+      'map_meta_cap' => true,
+    );
+  }
 
-	register_post_type($slug, $args);
+  register_post_type($slug, $args);
 }
 
 function cpt_generator()
 {
-	// Home
-	cpt_factory('Slider', 'slider', true, 'home');
-	cpt_factory('Working Group', 'working-group', true, 'home');
-	cpt_factory('Home Resource', 'home-resource', true, 'home');
+  // Home
+  cpt_factory('Slider', 'slider', true, 'home');
+  cpt_factory('Working Group', 'working-group', true, 'home');
+  cpt_factory('Home Resource', 'home-resource', true, 'home');
 
-	// Resources
-	cpt_factory('Video', 'video', true, 'resources');
-	cpt_factory('Infographic', 'infographic', true, 'resources');
+  // Resources
+  cpt_factory('Video', 'video', true, 'resources');
+  cpt_factory('Infographic', 'infographic', true, 'resources');
 
-	// Advocacy
-	cpt_factory('Legislative Advocacy', 'legislative-advocacy', false, 'advocacy');
-	cpt_factory('Regulatory Advocacy', 'regulatory-advocacy', false, 'advocacy');
+  // Advocacy
+  cpt_factory('Legislative Advocacy', 'legislative-advocacy', false, 'advocacy');
+  cpt_factory('Regulatory Advocacy', 'regulatory-advocacy', false, 'advocacy');
 
-	// About
-	cpt_factory('Annual Report', 'annual-report', true, 'about');
-	cpt_factory('Guiding Principle', 'guiding-principle', true, 'about');
-	cpt_factory('Leadership', 'leadership', false, 'about');
+  // About
+  cpt_factory('Annual Report', 'annual-report', true, 'about');
+  cpt_factory('Guiding Principle', 'guiding-principle', true, 'about');
+  cpt_factory('Leadership', 'leadership', false, 'about');
 
-	// Individual CPTs
-	cpt_factory('Event', 'event');	// Events
-	cpt_factory('Survey', 'survey');	// Surveys
-	cpt_factory('Co-pay Resource', 'copay');	// Co-pay Accumulator Resources
-	cpt_factory('Coalition', 'coalition');	// Coalitions
+  // Individual CPTs
+  cpt_factory('Event', 'event');  // Events
+  cpt_factory('Survey', 'survey');  // Surveys
+  cpt_factory('Co-pay Resource', 'copay');  // Co-pay Accumulator Resources
+  cpt_factory('Coalition', 'coalition');  // Coalitions
 
-	// Custom Backpages  & One-offs
-	cpt_factory('Custom Page', 'backpage');
-	cpt_factory('COVID-19 Resource', 'covid-19'); // COVID-19 Resources
-	cpt_factory('ICER Resource', 'icer-resource', true, 'icer'); // ICER Resources
-	cpt_factory('ICER Speaker', 'icer-speaker', true, 'icer'); // ICER Speakers
+  // Custom Backpages  & One-offs
+  cpt_factory('Custom Page', 'backpage');
+  cpt_factory('Neutropenia Resource', 'neutropenia', true); // Neutropenia (prev. COVID-19) Resources
+  cpt_factory('Asthma Resource', 'asthma-resource', true, 'icer'); // Asthma (prev. ICER) Resources
+  cpt_factory('ICER Resource', 'icer-resource', true, 'icer'); // ICER Resources
+  // cpt_factory('ICER Speaker', 'icer-speaker', true, 'icer'); // ICER Speakers // NOTE no longer in use
 }
 add_action('init', 'cpt_generator');
